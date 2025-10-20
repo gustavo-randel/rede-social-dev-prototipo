c15cec942add15c5da572616c38e4d2729dcf44c
Gustavo Randel <137479113+gustavo-randel@users.noreply.github.com>
2025-10-20 20:08:52 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 import './scss/styles.scss'
 
+import { Dropdown, Modal } from 'bootstrap'
+
+// new post
+document.getElementById('newPostBtnInput').addEventListener('click', () => {
+  const modalElement = document.getElementById('postModal')
+  const postModal = new Modal(modalElement)
+  postModal.show()
+  setTimeout(() => {
+    document.getElementById('writePostInput').focus()
+  }, 500)
+})
 
 // theme switcher
 const themeSwitcher = document.getElementById('theme-switcher');
 themeSwitcher.addEventListener('click', () => {
+   const iconElement = document.querySelector('#theme-switcher>i')
     if (document.getElementById('theme').getAttribute('data-bs-theme') === 'dark') {
         document.getElementById('theme').setAttribute('data-bs-theme', 'light')
+        iconElement.classList.remove('bi-moon-stars')
+        iconElement.classList.add('bi-brightness-high')
+
     } else {
         document.getElementById('theme').setAttribute('data-bs-theme', 'dark');  
+        iconElement.classList.remove('bi-brightness-high')
+        iconElement.classList.add('bi-moon-stars')
+
     }
 });
